810685b0dcf4eb92621d5327315aecfe5de5172c
danielvp456 <danielvp456@hotmail.com>
2024-10-05 16:08:07 -0500

    all prediction services are now working as expected

--- app/api/nasaApi.py
+++ app/api/nasaApi.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 @router.post("/trainModel/")
 def train_model(longitude: float, latitude: float):
-    return NasaService.get_power_api(20240101, 20240929, longitude, latitude, 'daily', 'csv')
+    return NasaService.get_power_api(20180101, 20241003, longitude, latitude, 'daily', 'csv')
 
 @router.get("/tomorrowPrediction")
 def tomorrow_prediction(longitude: float, latitude: float):
@@ -16,15 +16,15 @@ def tomorrow_prediction(longitude: float, latitude: float):
 
 @router.get("/weekPrediction")
 def week_prediction(longitude: float, latitude: float):
-    result = PredictService.predict_daily(longitude, latitude)
+    result = PredictService.much_days_prediction(longitude, latitude, 15, 7)
     return result
 
 @router.get("/monthPrediction")
 def month_prediction(longitude: float, latitude: float):
-    result = PredictService.predict_daily(longitude, latitude)
+    result = PredictService.much_days_prediction(longitude, latitude, 30, 30)
     return result
 
 @router.get("/cuarterPrediction")
 def cuarter_prediction(longitude: float, latitude: float):
-    result = PredictService.predict_daily(longitude, latitude)
+    result = PredictService.much_days_prediction(longitude, latitude, 90, 90)
     return result
--- app/services/predictService.py
+++ app/services/predictService.py
@@ -30,7 +30,47 @@ class PredictService:
         
         return {'error': False, 'predictions': all_predictions}
 
+    def much_days_prediction(longitude, latitude, sequence_length, prediction_number):
+        if not NasaService.evaluate_if_model_exists(longitude, latitude):
+            return {'error': True, 'message': "Los modelos no existen"}
         
+        parameters = ["T2M", "PRECTOTCORR", "WS10M", "RH2M"]
+        all_predictions = []
+        for param in parameters:
+            csv_folder_path = 'csvFiles'
+            model_folder_path = 'IA_Models'
+
+            csv_file_name = f"{longitude:.6f}_{latitude:.6f}.csv"
+            csv_full_path = os.path.join(csv_folder_path, csv_file_name)
+
+            model_file_name = f"{longitude:.6f}_{latitude:.6f}_{param}.keras"
+            model_full_path = os.path.join(model_folder_path, model_file_name)
+
+            series = DataPreprocessor.get_series(csv_full_path, param)
+            predictions = PredictModel.prediction(model_full_path, series['series'], sequence_length, prediction_number)
+            #PredictModel.show_predictions(next_week_predictions)
+            result_safe = [float(pred) for pred in predictions]
+            all_predictions.append(PredictService.prediction_average(result_safe))
+        
+        return {'error': False, 'predictions': all_predictions}
+    
+    
+
+    def prediction_average(lst):
+        if not lst:
+            return {'min': None, 'max': None, 'average': None}  # Manejo de caso si la lista está vacía
+        
+        print("Average for: ", len(lst))
+
+        min_value = min(lst)  # Obtener el valor mínimo
+        max_value = max(lst)  # Obtener el valor máximo
+        average_value = sum(lst) / len(lst)  # Calcular el promedio
+
+        return {
+            'min': min_value,
+            'max': max_value,
+            'average': average_value
+        }
 
     def predict_daily(longitude, latitude):
         longitude = float(longitude)
